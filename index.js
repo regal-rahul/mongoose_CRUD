8b56c0f5b2e4169794f0305c5a8be023ce8a4bd8
regal-rahul <regalrahul777@gmail.com>
2022-06-02 19:24:34 +0530

    Updates products show and added some dummy products

--- index.js
+++ index.js
@@ -17,10 +17,18 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('WOOF!')
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
 })
 
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    console.log(product);
+    res.render('products/show', { product })
+})
 
 app.listen(3000, () => {
     console.log("Listening to port 3000!")
--- seeds.js
+++ seeds.js
@@ -10,16 +10,51 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.log(err)
     })
 
-const p = new Product({
-    name: 'Litchi',
-    price: 10,
-    category: 'fruit'
-})
+// const p = new Product({
+//     name: 'Litchi',
+//     price: 10,
+//     category: 'fruit'
+// })
 
-p.save()
-.then(p => {
-    console.log(p)
-})
-.catch(e => {
-    console.log(e)
-})
+// p.save()
+// .then(p => {
+//     console.log(p)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Golden Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
